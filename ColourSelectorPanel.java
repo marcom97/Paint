7abed2a67bc753cb3394150ec6ec744143652e72
Teammate1 <Teammate1>
2018-11-04 14:45:09 -0500

    partially done

--- ColourSelectorPanel.java
+++ ColourSelectorPanel.java
@@ -1,10 +1,13 @@
 package ca.utoronto.utm.paint;
 
 import javafx.event.ActionEvent;
+
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+
+
 
 
 public class ColourSelectorPanel extends GridPane implements EventHandler<ActionEvent> {
@@ -13,21 +16,25 @@ public class ColourSelectorPanel extends GridPane implements EventHandler<Action
 
 	public ColourSelectorPanel(View view) {
 		this.view = view;
-		
-		String[] buttonLabels = {"red", "blue", "yellow", "black" , "orange", "Green", "White", "Pink", "Purple"};
+		String[] buttonLabels = {"red", "blue", "yellow", "black" , "orange", "Green", "White", "Pink", "Purple", "aliceblue", "Aqua",
+				"beige", "brown", "darkgray","crimson", "darkgreen","violet","chocolate"};
 		int row = 0;
 		for (String label : buttonLabels) {
 			Button button = new Button(label);
-			button.setMinWidth(02);
+			button.setMinSize(30, 1);
 			this.add(button, row, 0);
+			button.setTextFill(null);
+			button.setStyle("-fx-background-color:" + label);
 			row++;
 			button.setOnAction(this);
 		}
+		
+		
 	}
-
+	
 	@Override
 	public void handle(ActionEvent event) {
-		String command = ((Button) event.getSource()).getText();
+		String command = "Selects Color" + " " +((Button) event.getSource()).getText();
 		this.view.getPaintPanel().setMode(command);
 		System.out.println(command);
 	}
