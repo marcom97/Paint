106033c7dc45f965da6ef622447f43bd64abd403
Marco Matamoros <marcomatamoros97@gmail.com>
2018-11-13 13:48:57 -0500

    Change alignment of FillStylePanel and its children

--- FillStylePanel.java
+++ FillStylePanel.java
@@ -2,30 +2,33 @@ package ca.utoronto.utm.paint;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
-public class FillStylePanel extends GridPane implements EventHandler<ActionEvent> {
+public class FillStylePanel extends VBox implements EventHandler<ActionEvent> {
 	private View view;
 	
 	public FillStylePanel(View view) {
 		this.view = view;
+		this.setAlignment(Pos.TOP_CENTER);
 		
 		final ToggleGroup group = new ToggleGroup();
 		RadioButton solidButton = new RadioButton("Solid");
 		solidButton.setToggleGroup(group);
 		solidButton.setSelected(true);
 		solidButton.setOnAction(this);
-		solidButton.setMinWidth(100);
+		solidButton.setMaxWidth(90);
 		
 		RadioButton outlineButton = new RadioButton("Outline");
 		outlineButton.setToggleGroup(group);
 		outlineButton.setOnAction(this);
-		outlineButton.setMinWidth(100);
+		outlineButton.setMaxWidth(90);
 		
-		this.add(solidButton, 0, 0);
-		this.add(outlineButton, 0, 1);
+		this.getChildren().add(solidButton);
+		this.getChildren().add(outlineButton);
 	}
 
 	@Override
