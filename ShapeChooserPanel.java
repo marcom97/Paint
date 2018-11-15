c11522512dfc8c7b7f8cff4fb7892a17f1eeb5f1
Teammate2 <Teammate2>
2018-11-15 13:31:08 -0500

    Finished Bug1 and fixed error

--- ShapeChooserPanel.java
+++ ShapeChooserPanel.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
 
 public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEvent> {
 
@@ -14,7 +16,7 @@ public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEv
 		this.view = view;
 
 		String[] buttonLabels = { "Circle", "Rectangle", "Square", "Squiggle", "Polyline" };
-
+		
 		int row = 0;
 		for (String label : buttonLabels) {
 			Button button = new Button(label);
@@ -22,7 +24,62 @@ public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEv
 			this.add(button, 0, row);
 			row++;
 			button.setOnAction(this);
+			
+			if (label == "Circle") {
+				javafx.scene.shape.Circle circle = new javafx.scene.shape.Circle();
+				circle.setRadius(10f);
+				circle.setFill(Color.WHITE);
+				circle.setStroke(Color.BLACK);
+				button.setGraphic(circle);
+				button.setText(null);
+			}
+			else if (label == "Rectangle"){
+				javafx.scene.shape.Rectangle rectangle = new javafx.scene.shape.Rectangle();
+				rectangle.setWidth(20);
+				rectangle.setHeight(12);
+				rectangle.setFill(Color.WHITE);
+				rectangle.setStroke(Color.BLACK);
+				button.setGraphic(rectangle);
+				button.setText(null);
+
+			}
+			else if (label == "Square") {
+				javafx.scene.shape.Rectangle square = new javafx.scene.shape.Rectangle();
+				square.setWidth(15);
+				square.setHeight(15);
+				square.setFill(Color.WHITE);
+				square.setStroke(Color.BLACK);
+				button.setGraphic(square);
+				button.setText(null);
+
+			}
+			else if (label == "Squiggle") {
+				javafx.scene.shape.Arc arc = new javafx.scene.shape.Arc();
+				arc.setCenterX(5.0f);
+				arc.setCenterY(0.0f);
+				arc.setRadiusX(10.0f);
+				arc.setRadiusY(10.0f);
+				arc.setStartAngle(0.0f);
+				arc.setLength(180.0f);
+				arc.setType(ArcType.OPEN);
+				arc.setStroke(Color.BLACK);
+				arc.setFill(null);
+				
+				button.setGraphic(arc);
+				button.setText(null);
+
+				
+			}
+			else if (label == "Polyline") {
+				javafx.scene.shape.Polyline poly = new javafx.scene.shape.Polyline();
+				poly.getPoints().addAll(new Double[] {0.0,5.0,10.0,10.0,10.0,20.0,20.0,24.0});
+				poly.setStroke(Color.BLACK);
+				button.setGraphic(poly);
+				button.setText(null);
+
+			}
 		}
+		
 	}
 
 	@Override
