031eeaf54fae6dd74de6bcf7e414cfe0b0e0a4aa
Marco Matamoros <marcomatamoros97@gmail.com>
2018-11-15 22:39:08 -0500

    Add documentation for ShapeDrawingCommand

--- ShapeDrawingCommand.java
+++ ShapeDrawingCommand.java
@@ -2,6 +2,11 @@ package ca.utoronto.utm.paint;
 
 import javafx.scene.canvas.GraphicsContext;
 
+/**
+ * The base class for all shape drawing commands
+ * @author Marco Matamoros
+ *
+ */
 public abstract class ShapeDrawingCommand implements DrawingCommand {	
 	@Override
 	public void execute(GraphicsContext g) {
@@ -12,8 +17,19 @@ public abstract class ShapeDrawingCommand implements DrawingCommand {
 			this.drawOutlined(g);
 		}
 	}
-	
+	/**
+	 * Draw a shape that is filled using the supplied GraphicsContext
+	 * @param g the context used to draw
+	 */
 	protected abstract void drawFilled(GraphicsContext g);
+	/**
+	 * Draw a shape that is outlined using the supplied GraphicsContext
+	 * @param g the context used to draw
+	 */
 	protected abstract void drawOutlined(GraphicsContext g);
+	/**
+	 * Return the Shape object that will be drawn
+	 * @return the shape to be drawn
+	 */
 	protected abstract Shape getShape();
 }
